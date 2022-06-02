a8525b0421987589976f7b9c4948a080b0a85545
Areebuddin Phundreimayum <areeb6162123@gmail.com>
2022-06-03 05:26:10 +0530

    Added tooltip

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import navBars from "./navBars";
 import { useState } from "react";
-import ReactHover, { Hover, Trigger } from "react-hover/dist/ReactHover";
 import "./nav.css";
 
 const Nav = () => {
 	const [active, setActive] = useState("#home");
-	const [display, setDisplay] = useState({ display: "none" });
 	const setActiveClass = (state) => {
 		if (active === state) return "active";
 		return "";
-	};
-	const options = {
-		followCursor: true,
-		shiftX: 20,
-		shiftY: 0,
 	};
 	const capitalise = (string) => {
 		return string[0].toUpperCase() + string.slice(1).toLowerCase();
@@ -24,25 +17,17 @@ const Nav = () => {
 		<nav>
 			{navBars.map(([id, icon]) => {
 				return (
-					<ReactHover key={id} options={options}>
-						<Trigger type="trigger">
-							<a
-								key={id}
-								href={`#${id}`}
-								onClick={() => setActive(`#${id}`)}
-								className={setActiveClass(`#${id}`)}
-								onMouseEnter={() => setDisplay("inline")}
-								onMouseLeave={() => setDisplay("none")}
-							>
-								{icon}
-							</a>
-						</Trigger>
-						<Hover type="hover">
-							<span className="nav__title" style={{ display: display }}>
-								{capitalise(id)}
-							</span>
-						</Hover>
-					</ReactHover>
+					<div className="tooltip" key={id}>
+						<a
+							key={id}
+							href={`#${id}`}
+							onClick={() => setActive(`#${id}`)}
+							className={setActiveClass(`#${id}`)}
+						>
+							{icon}
+						</a>
+						<span className="tooltiptext">{capitalise(id)}</span>
+					</div>
 				);
 			})}
 		</nav>
